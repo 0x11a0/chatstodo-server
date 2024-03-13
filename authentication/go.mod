ec758458a0e75e845abf84e9e3a14d4c69ce4a2c
ang-rui-yan <angruiyan@gmail.com>
2024-03-13 15:05:46 +0800

    added cobra command to migrate and added oauth callback

--- authentication/go.mod
+++ authentication/go.mod
@@ -5,8 +5,10 @@ go 1.21.3
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.9.1
+	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/spf13/cobra v1.8.0
 )
 
 require (
@@ -26,6 +28,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.19.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -33,6 +36,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
 	golang.org/x/arch v0.7.0 // indirect
--- authentication/main.go
+++ authentication/main.go
@@ -7,14 +7,17 @@ import (
 	"os"
 	"time"
 
+	cmd "chatstodo/authentication/cmd"
+
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var (
-	jwtKey = []byte("your_secret_key")
+	jwtKey []byte
 	db *sql.DB
 	postgresqlAddress string
 )
@@ -31,73 +34,86 @@ func main() {
 	godotenv.Load("../.env")
 	postgresqlAddress = os.Getenv("USER_POSTGRESQL_URL")
 
-	// const BaseURL = "http://localhost:8000"
-	// goth.UseProviders(
-	// 	google.New(os.Getenv("GPLUS_KEY"), os.Getenv("GPLUS_SECRET"), BaseURL+"/auth/google/callback"),
-	// )
+	jwtSecretKey := os.Getenv("JWT_SECRET_KEY")
+	jwtKey = []byte(jwtSecretKey)
 
+	cmd.Execute(postgresqlAddress)
+
+	
     // Connect to PostgreSQL
     connStr := postgresqlAddress
     db, err = sql.Open("postgres", connStr)
     if err != nil {
         log.Fatal(err)
     }
+
     // Initialize Gin
     r := gin.Default()
 
-	// ping health including db
-	r.GET("/health", func(c *gin.Context) {
+	authGroup := r.Group("/auth")
+	{
+		// ping health including db
+		authGroup.GET("/health", func(c *gin.Context) {
 
-		err = db.PingContext(c)
-		if err != nil {
-			c.JSON(http.StatusFailedDependency, "db is down")
-			return
-		}
+			err = db.PingContext(c)
+			if err != nil {
+				c.JSON(http.StatusFailedDependency, "db is down")
+				return
+			}
 
-		c.JSON(http.StatusOK, "Service is running")
-	})
-    // OAuth callback endpoint
-    // r.GET("/oauth/callback", handleOAuthCallback)
+			c.JSON(http.StatusOK, "Service is running")
+		})
 
-    // Start the server
-    r.Run(":8080") // Listen and serve on 0.0.0.0:8080
+		// OAuth callback endpoint
+		authGroup.POST("/oauth/callback", handleOAuthCallback)
+	}
+
+    r.Run(":8080")
 }
 
-// func handleOAuthCallback(c *gin.Context) {
-// 	user, err := gothic.CompleteUserAuth(c.Writer, c.Request)
-// 	if err != nil {
-// 		c.AbortWithError(http.StatusInternalServerError, err)
-// 		return
-// 	}
+func handleOAuthCallback(c *gin.Context) {
+	var requestBody struct {
+		Email     string `json:"email"`
+	}
 
+	if err := c.ShouldBindJSON(&requestBody); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Invalid JSON request"})
+		log.Printf("%e",err)
+		return
+	}
 
-//     var userID int
-//     err = db.QueryRow("SELECT id FROM users WHERE email = $1", user.Email).Scan(&userID)
-//     if err == sql.ErrNoRows {
-//         // User doesn't exist, insert new record
-//         err = db.QueryRow("INSERT INTO users(email) VALUES($1) RETURNING id", user.Email).Scan(&userID)
-//         if err != nil {
-//             c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating user"})
-//             return
-//         }
-//     } else if err != nil {
-//         c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
-//         return
-//     }
+	var userId string
+	if err := db.QueryRow("SELECT id FROM users WHERE email = $1", requestBody.Email).Scan(&userId); err != nil {
+		if err == sql.ErrNoRows {
+			// User doesn't exist, insert new record
+			newUUID := uuid.New().String()
+			err = db.QueryRow("INSERT INTO users(id, email) VALUES($1, $2) RETURNING id", newUUID, requestBody.Email).Scan(&userId)
+            if err != nil {
+                c.JSON(http.StatusInternalServerError, gin.H{"error": "Error creating user"})
+				log.Printf("%e",err)
+                return
+            }
+		} else if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Database error"})
+			log.Printf("%e",err)
+			return
+		}
+	}
 
-//     // Generate JWT
-//     tokenString, err := generateJWT(userID, user.Email)
-//     if err != nil {
-//         c.JSON(http.StatusInternalServerError, gin.H{"error": "Error signing token"})
-//         return
-//     }
+    // Generate JWT
+    tokenString, err := generateJWT(userId, requestBody.Email)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error signing token"})
+		log.Printf("%e",err)
+        return
+    }
 
-//     // Send the token back to the user
-//     c.JSON(http.StatusOK, gin.H{"token": tokenString})
-// }
+    // Send the token back to the user
+    c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
 
 // Function to generate JWT
-func generateJWT(userID int, email string) (string, error) {
+func generateJWT(userID string, email string) (string, error) {
     token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
         "user_id": userID,
         "email":   email,
