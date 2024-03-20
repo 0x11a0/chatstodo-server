4dce34beccc45bd12b42ef875fe762b5f172079d
lucasodra <lucas@11a0.com>
2024-03-20 22:18:40 +0800

    updated platform controller get function

--- user-manager/controllers/PlatformController.js
+++ user-manager/controllers/PlatformController.js
@@ -7,27 +7,21 @@ const { sequelize } = require('../services/db');
 
 // Controller methods
 const PlatformController = {
-  // Add a new platform
   addPlatform: async (req, res) => {
     const { platformName, credentialId, credentialSecret } = req.body;
 
     try {
-      // Start a database transaction
       const transaction = await sequelize.transaction();
 
       try {
-        // Create the credential within the transaction
         const newCredential = await Credential.create({ credentialId, credentialSecret }, { transaction });
 
-        // Create the platform using the generated credentialId within the transaction
         const newPlatform = await Platform.create({ platformName, credentialId: newCredential.id, UserId: req.userId}, { transaction });
 
-        // Commit the transaction if everything is successful
         await transaction.commit();
 
         res.status(201).json(newPlatform);
       } catch (error) {
-        // Rollback the transaction if any error occurs
         await transaction.rollback();
         console.error('Error adding platform:', error);
         res.status(500).json({ error: 'Error adding platform' });
@@ -43,35 +37,28 @@ const PlatformController = {
     const userId = req.userId; // Retrieve the userId from req
 
     try {
-        // Start a database transaction
         const transaction = await sequelize.transaction();
 
         try {
-            // Find the platform within the transaction
             const platform = await Platform.findByPk(platformId, { transaction });
             if (!platform) {
                 await transaction.rollback();
                 return res.status(404).json({ error: 'Platform not found' });
             }
 
-            // Check if the requesting user owns the platform
             if (platform.userId !== userId) {
                 await transaction.rollback();
                 return res.status(403).json({ error: 'Unauthorized: You do not own this platform' });
             }
 
-            // Find and remove the linked credential within the transaction
             await Credential.destroy({ where: { id: platform.credentialId } }, { transaction });
 
-            // Remove the platform
             await platform.destroy({ transaction });
 
-            // Commit the transaction if everything is successful
             await transaction.commit();
 
             res.status(204).end();
         } catch (error) {
-            // Rollback the transaction if any error occurs
             await transaction.rollback();
             console.error('Error removing platform:', error);
             res.status(500).json({ error: 'Error removing platform' });
@@ -86,9 +73,8 @@ const PlatformController = {
       const userId = req.userId; // Retrieve the userId from req
 
       try {
-          // Find all platforms belonging to the requesting user and include their associated credential
           const platforms = await Platform.findAll({
-              where: { userId }, // Filter by userId
+              where: { UserId: userId }, // Filter by userId
               include: {
                   model: Credential,
                   as: 'Credential' // Alias for the associated credential
--- user-manager/middleware/auth.js
+++ user-manager/middleware/auth.js
@@ -4,48 +4,34 @@ const User = require('../models/User');
 
 console.log(process.env.JWT_SECRET);
 
-// Middleware to check if the user is authenticated
 exports.isAuthenticated = async (req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(401).send({ error: "No authorization token provided." });
   }
 
-  // Get the 'authorization' header from the incoming request
   const authHeader = req.headers.authorization;
 
-  // Check if the authorization header exists. If it does, split it at the space
-  // (typically it looks like "Bearer <TOKEN>"). Extract the token part.
   const token = authHeader && authHeader.split(" ")[1];
 
-  // If there's no token, it means the request doesn't have proper authentication details
   if (!token) {
     // Return a 401 (Unauthorized) status with a relevant message
     return res.status(401).json({ message: "unauthorized" });
   }
 
   try {
-    // If there's a token, attempt to verify it using the JWT secret from environment variables
-    // If the verification is successful, decodedData will have the payload of the JWT.
-    // If it's not successful (e.g., if the token has been tampered with),
-    // it will throw an error, which you'd ideally catch in a production setup.
+
     const decodedData = jwt.verify(token, process.env.JWT_SECRET);
   
-    // Check if the user with the decoded ID exists in the database
     let user = await User.findOne({ where: { _id: decodedData.userId } });
 
-    // If the user doesn't exist, add a record for the user in the database
     if (!user) {
-      // Create a new user record with the decoded user ID
       user = await User.create({ _id: decodedData.userId });
     }
     req.userId = user._id;
-    // Attach the token to the request object for subsequent middleware/controllers
     req.token = token;
 
-    // Attach the user object to the request object as 'user'
     req.user = user;
 
-    // Move to the next middleware or route handler
     next();
   } catch (error) {
     console.error('Error verifying token:', error);
